548385b02ae74ac7fcc50da66794573c31cd4c86
sARASWAN-sINHA <saraswan1999@gmail.com>
2024-04-16 16:58:20 +0530

    BUG FIX: Updated the screen to show correct username FEAT: Added subscription type of user to the dash board screen.

--- frontend/src/pages/Dashboard/ClientDashboard.jsx
+++ frontend/src/pages/Dashboard/ClientDashboard.jsx
@@ -1,11 +1,19 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import DashboardCard from "../../components/Card/DashboardCard";
 import { userContext } from "../../context";
 import { useSelector } from "react-redux";
 
 const ClientDashboard = () => {
-  // const [loggedInUser, setLoginUser] = useContext(userContext)
-  const loggedInUser = useSelector(state => state.userState.user);
+  const userReduxState = useSelector(state => state.userState.user);
+  const userContextState = useContext(userContext)[0];
+  const initialUserState = userReduxState.id != userContextState.id ? { ...userContextState } : { ...userReduxState }
+  const [loggedInUser, _] = useState(initialUserState);
+
+  const userSubscription = loggedInUser.subscription;
+  const subscriptionMessage = userSubscription ?
+    userSubscription == "STD" ? "Standard" : "Premium"
+    : "None";
+
   return (
     <div className="h-[80vh] flex flex-col justify-center items-center gap-10">
       <DashboardCard
@@ -14,7 +22,7 @@ const ClientDashboard = () => {
           "As a client you will have the ability to browse through standard and premium articles."
         }
       />
-      <DashboardCard title={"subscription status:"} message={"none"} />
+      <DashboardCard title={"subscription status:"} message={subscriptionMessage.toUpperCase()} />
     </div>
   );
 };
