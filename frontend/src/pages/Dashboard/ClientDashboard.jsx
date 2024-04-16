b5dfc513f344665a3dbffaf4effe828fac07610a
sARASWAN-sINHA <saraswan1999@gmail.com>
2024-04-16 16:55:16 +0530

    BUG FIX: Updated the screen to show correct username FEAT: Added subscription type of user to the dash board screen,

--- frontend/src/pages/Dashboard/ClientDashboard.jsx
+++ frontend/src/pages/Dashboard/ClientDashboard.jsx
@@ -1,11 +1,14 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import DashboardCard from "../../components/Card/DashboardCard";
 import { userContext } from "../../context";
 import { useSelector } from "react-redux";
 
 const ClientDashboard = () => {
-  // const [loggedInUser, setLoginUser] = useContext(userContext)
-  const loggedInUser = useSelector(state => state.userState.user);
+  const userReduxState = useSelector(state => state.userState.user);
+  const userContextState = useContext(userContext)[0];
+  const updateUserContext = useContext(userContext)[1];
+  const initialUserState = userReduxState.id != userContextState.id ? { ...userContextState } : { ...userReduxState }
+  const [loggedInUser, _] = useState(initialUserState);
   return (
     <div className="h-[80vh] flex flex-col justify-center items-center gap-10">
       <DashboardCard
